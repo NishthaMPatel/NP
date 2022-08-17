2f8f37447c4c07098cca2da9d0b35db0361208f0
Nishtha Patel <nishthapatel0502@gmail.com>
2022-08-17 18:24:08 +0530

    p1_1

---
+++ src/main/kotlin/p1_1.kt
@@ -0,0 +1,21 @@
+fun main(){
+    val a: Int = 10
+    val b: Double = 15.5
+    val c: String = "hello"
+    val d: Char = '$'
+    val e: Boolean = true
+    val f: Float = 1.5f
+    val g: Long = 9999999L
+    val h: Short = 5000
+    val i: Byte = 100
+
+    println("Integer value:- "+a)
+    println("Double value:-"+b)
+    println("String value:-"+c)
+    println("Character value:-"+d)
+    println("Boolean Value:_"+e)
+    println("Float Value:-"+f)
+    println("Long Value:-"+g)
+    println("Short Value:-"+h)
+    println("Byte Value:-"+i)
+}
