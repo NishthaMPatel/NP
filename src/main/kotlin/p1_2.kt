8d9f753207c42e24b262461eb9218d7578bae820
Nishtha Patel <nishthapatel0502@gmail.com>
2022-08-17 18:28:46 +0530

    p1_2

---
+++ src/main/kotlin/p1_2.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    val a: Int = 10
+    val b: Double = a.toDouble()
+    val c: String = "20"
+    val d: Int = c.toInt()
+    val e: Double = c.toDouble()
+
+    println("Integer Value:-" + a)
+    println("Double Value (from Integer) :- " + b)
+    println("String Value :- " + c)
+    println("Integer Value (from String) :-" + d)
+    println("Double Value (from String) :-" + e)
+}
+
+
